4512bcdc978790fdd1b09bd9f429e30327c2372d
Lucia Barrela <lucia.sofiabb@gmail.com>
2023-12-22 12:31:15 +0000

    Created

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Lucia Barrela on 22/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
