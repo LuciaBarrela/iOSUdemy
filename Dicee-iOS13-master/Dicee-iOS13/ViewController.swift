ac44eed014908f0cbedb0d7c1587bba05993f584
Lucia Barrela <lucia.sofiabb@gmail.com>
2023-12-26 18:46:24 +0000

    update

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -2,19 +2,30 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //dado 1
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    //dado 2
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.alpha = 0.5
+        diceImageView2.alpha = 0.5
     }
 
-
+    //Roll button
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+    }
+    
 }
 
