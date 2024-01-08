ba52796bfb06a38936363cc7c101f0db6276ac04
Lucia Barrela <lucia.sofiabb@gmail.com>
2024-01-08 12:48:47 +0000

    inicio

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+    }
+    
 }
