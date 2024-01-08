fb25e714f94d278b463c8389908ff5086c15b127
Lucia Barrela <lucia.sofiabb@gmail.com>
2024-01-08 16:47:19 +0000

    struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,2 +1,25 @@
 
+struct Town {
+    let name = "Tinytown"
+    var citizens = ["Lucia", "Miguel"]
+    var resources = ["Grain": 100, "Ore": 42, "Wool": 75] //dictionary
+    
+    //if it's inside a struct or class.. it's a method
+    //but if it's just hanging around at the end of the code it's a function
+    func fortify() {
+        print("Defenses increased!")
+        
+    }
+}
+
+var myTown = Town()
+
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grain.")
+
+myTown.citizens.append("Keanu Reeves")
+print(myTown.citizens)
+
+
+
 
--- Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+    
+            ["Four + Two is equal to Six.", "True"],
+            ["Five - Three is greater than One.", "True"],
+            ["Three + Eight is less than Ten.", "False"]
+    
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       updateUI()
     }
 
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle //True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("right")
+        } else {
+            print("wrong")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+            
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
