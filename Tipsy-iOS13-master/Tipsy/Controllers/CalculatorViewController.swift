bb92976ab14afb849aa2327042913b42c4621a0c
Lucia Barrela <lucia.sofiabb@gmail.com>
2024-01-22 16:02:08 +0000

    finished

---
+++ Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+        var numberOfPeople = 2
+        var billTotal = 0.0
+        var finalResult = "0.0"
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        //Dismiss the keyboard when the user chooses one of the tip values.
+               billTextField.endEditing(true)
+               
+               zeroPctButton.isSelected = false
+               tenPctButton.isSelected = false
+               twentyPctButton.isSelected = false
+               sender.isSelected = true
+               
+               let buttonTitle = sender.currentTitle!
+               let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+               let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+               tip = buttonTitleAsANumber / 100
+        
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+                numberOfPeople = Int(sender.value)    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bill = billTextField.text!
+               if bill != "" {
+                   billTotal = Double(bill)!
+                   let result = billTotal * (1 + tip) / Double(numberOfPeople)
+                   finalResult = String(format: "%.2f", result)
+               }
+               
+               //In Main.storyboard there is a segue between CalculatorVC and ResultsVC with the identifier "goToResults".
+               //This line triggers the segue to happen.
+               self.performSegue(withIdentifier: "goToResults", sender: self)
+           }
+           
+           //This method gets triggered just before the segue starts.
+           override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+               
+               //If the currently triggered segue is the "goToResults" segue.
+               if segue.identifier == "goToResults" {
+                   
+                   //Get hold of the instance of the destination VC and type cast it to a ResultViewController.
+                   let destinationVC = segue.destination as! ResultsViewController
+                   
+                   //Set the destination ResultsViewController's properties.
+                   destinationVC.result = finalResult
+                   destinationVC.tip = Int(tip * 100)
+                   destinationVC.split = numberOfPeople
+               }
+           }
+       }
