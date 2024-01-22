1ea27a8876570fee9f3dddff534b0be4e74169f1
Lucia Barrela <lucia.sofiabb@gmail.com>
2024-01-22 15:22:09 +0000

    done

--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
 
     @IBOutlet weak var bmiLabel: UILabel!
     
@@ -20,6 +22,8 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
 
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Lucia Barrela on 22/01/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
