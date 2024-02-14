6b14db3280da054b0c4277e8e5963abde47c3c1f
Lucia Barrela <lucia.sofiabb@gmail.com>
2024-02-14 10:26:31 +0000

    latest update

---
+++ Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+            
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+}
+
